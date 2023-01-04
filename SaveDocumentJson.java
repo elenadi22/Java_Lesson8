f1ebef83bd6b241e0db1b28f92cba4e8197c08a5
elenadi22 <106550973+elenadi22@users.noreply.github.com>
2023-01-05 00:21:25 +0300

    Create SaveDocumentJson.java

---
+++ SaveDocumentJson.java
@@ -0,0 +1,50 @@
+package Lesson8;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.Scanner;
+
+public class SaveDocumentJson extends SaveDocument {
+    public SaveDocumentJson() {
+        super.scan = new Scanner(System.in);
+    }
+
+    @Override
+    void EnterSaveDoс(String nameDoc) {
+        try {
+            String expansion = ".json";
+            if (new File(nameDoc + expansion).isFile() != true) {
+                Writer fw = new FileWriter(nameDoc + expansion);
+                String input = scan.nextLine();
+                fw.write(input);
+                fw.close();
+            } else {
+                Writer fw = new FileWriter(nameDoc + expansion, true);
+                String input = scan.nextLine();
+                fw.append("\n" + input);
+                fw.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Произошла неведомая ошибка!");
+        }
+    }
+
+    @Override
+    void LineSaveDoс(String nameDoc, String line) {
+        try {
+            String expansion = ".json";
+            if (new File(nameDoc + expansion).isFile() != true) {
+                Writer fw = new FileWriter(nameDoc + expansion);
+                fw.write(line);
+                fw.close();
+            } else {
+                Writer fw = new FileWriter(nameDoc + expansion, true);
+                fw.append("\n" + line);
+                fw.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Произошла неведомая ошибка!");
+        }
+    }
+}
